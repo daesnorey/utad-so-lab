04efb70629c28b65a54640442b91e4fb4aa5b214
= <=>
2019-11-11 19:11:55 -0500

    something

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once("backend.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba Sistemas Operativos</title>
+	<meta charset="UTF-8" >
+	<meta lang="es" >
+</head>
+<body>
+	<h2>Hello men</h2>
+</body>
+</html>
