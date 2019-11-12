5ea550f02c4998591552b56b8911523e4f9fec94
Daniel Esteban Novoa Reyes <dnovoare@everis.com>
2019-11-11 20:27:01 -0500

    algo

--- backend/login.php
+++ backend/login.php
@@ -4,6 +4,8 @@ function login($link, $data) {
     echo "entra a login";
     $username = $$data->username;
     $password = $$data->password;
+    echo $username;
+    echo $password;
     // $password = "1' OR 1 = 1 UNION ALL SELECT 1, table_name FROM information_schema.tables WHERE table_schema = database() OR '' = '33";
 
     $query = "SELECT username, password FROM users WHERE username = '$username' AND password = '$password';";
